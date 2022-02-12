f2e4f9745283d63f4feaa1ef5f2b9fae85b298b0
9k-ux <76711923+9k-ux@users.noreply.github.com>
2022-02-12 21:59:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 8000;
 const path = require("path");
+// const port = process.env.PORT || 8000;
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+// const path = require("path");
 const { traceDeprecation } = require("process");
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(express.json());
+var driver_name;
+
+var driver_age;
+var driver_number;
+var driver_trucknumber;
+var driver_truckcapacity;
+var driver_transportername;
+var driver_email;
+var driver_password;
+var driver_experience;
+var otp;
+
+
 
 
 mongoose
@@ -43,8 +60,9 @@ const getList = async () => {
     console.log(err);
   }
 };
-
+var dealers;
 getList();
+
 
 
 
@@ -64,7 +82,45 @@ const dealerSchema = new mongoose.Schema({
 
 const dealerData = new mongoose.model("dealers", dealerSchema);
 
+const detailschema = {
+  driver_name:String,
+  driver_age:String,
+  driver_number:Number,
+  driver_trucknumber:String,
+  driver_truckcapacity:String,
+  driver_transportername:String,
+  driver_email:String,
+  driver_password:String,
+  driver_experience:String,
+  // companypassword:String,
+  
 
+  };
+  const Driver = mongoose.model("Driver" , detailschema);
+  
+var transporter = nodemailer.createTransport({
+  service: '',
+  auth: {
+    user: 'kapilmehta634@gmail.com',
+    pass: 'Kapil@12345'
+  }
+});
+
+
+const getListDealers = async () => {
+  try {
+    const result = await dealerData
+      // .find({name:{$in:["anshu","ankit"]},number:{$gt:21}})
+      // .find({$or:[{name:"anshu"},{number:{$gt:21}}]})
+      .find();
+    // console.log(result);
+    dealers = result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getListDealers();
 
 app.use(express.static(path.join(__dirname, "assets")));
 
@@ -73,11 +129,23 @@ app.get("/", function (req, res) {
 
   res.render("index.ejs");
 });
+app.post("/otp", function (req, res) {
+  // const email=req.params.email;
+  var rotp1 = req.body.otp1;
+  var rotp2 = req.body.otp2;
+  var rotp3 = req.body.otp3;
+  var rotp4 = req.body.otp4;
+  var final = rotp1+rotp2+rotp3+rotp4;
+  if(final==otp){
+    console.log('correct');
+  }
+  
+});
 app.get("/dealer", function (req, res) {
   res.render("Dealer.ejs",{status:0,states:states});
 });
 app.get("/driver", function (req, res) {
-  res.render("Driver.ejs");
+  res.render("Driver.ejs",{states:states});
 });
 
 app.get("/otp/:email", function (req, res) {
@@ -87,6 +155,9 @@ app.get("/otp/:email", function (req, res) {
 
 app.get("/dealerDasboard", function (req, res) {
   res.render("dealerDashboard.ejs");
+});
+app.get("/driverDashboard", function (req, res) {
+  res.render("Drivers_Dashboard.ejs",{dealers:dealers});
 });
 app.post("/dealerSignUp", async (req, res)=> {
 
